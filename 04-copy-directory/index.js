b5cda3c56a9a85c591272ef7857dadd052c5cced
yar4ick <y.hapalov@gmail.com>
2022-05-22 23:22:03 +0300

    feat: copy directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs/promises");
 
+function copyDir() {
+  const dirPath = path.resolve(__dirname, "files");
+  const dirPathCopy = path.resolve(__dirname, `files-copy`);
+
+  fsPromises.mkdir(dirPathCopy, { recursive: true });
+
+  fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      return console.log("Unable to scan directory: " + err);
+    }
+
+    files.forEach((file) => {
+      fsPromises.copyFile(
+        path.join(dirPath, file.name),
+        path.join(dirPathCopy, file.name)
+      );
+    });
+  });
+}
+
+copyDir();
